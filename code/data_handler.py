1192633fb8627262d1aca0ee50ae83032cb947ec
Shashank Gupta <shashank.gupta@research.iiit.ac.in>
2016-05-04 14:52:06 +0530

    data handler modified

--- code/data_handler.py
+++ code/data_handler.py
@@ -3,6 +3,7 @@ from scipy.sparse import lil_matrix
 import numpy as np
 import pdb
 from scipy.io import loadmat
+import time
 
 class data_handler(object):
     def __init__(self, rating_path, trust_path):
@@ -10,29 +11,51 @@ class data_handler(object):
         self.trust_path = trust_path
 
     def load_matrices(self):
-       # R = lil_matrix((49291, 139739))
-       # W = lil_matrix((49291, 49291))
-       # f1 = open(self.rating_path)
-       # f2 = open(self.trust_path)
-       # for record in f1:
-       #     user_id, item_id, rating = map(lambda x:int(x), record.split())
-       #     R[user_id, item_id] = rating
-       # f1.close()
-       # for record in f2:
-       #     user1, user2, trust = map(lambda x:int(x), record.split())
-       #     W[user1, user2] = trust
         f = open(self.rating_path)
         f1 = open(self.trust_path)
         R = loadmat(f)
         W = loadmat(f1)
         R = R['rating_with_timestamp']
         W = W['trust']
-      #  pdb.set_trace()
-        return R, W
+        n_users = max(R[:, 0])
+        n_prod = max(R[:, 1])
+        n_cat = 6
+        cat_id = [7,8,9,10,11,19]
+        cat_map = {7:1, 8:2, 9:3, 10:4, 11:5, 19:6}
+        # Selecting records with category id falling in 6 pre-defined categories
+        # choosen in paper
+        R = R[np.in1d(R[:, 2], cat_id)]
+        R = R[R[:, 5].argsort()]
+        #pdb.set_trace()
+        pf = np.zeros((n_prod + 1, n_cat + 1))
+        for i in xrange(1, n_prod + 1):
+            cat = R[R[:, 1] == i, 2]
+            cat = map(lambda x:cat_map[x], cat)
+            pf[i, cat] = 1
+        # Sparse matrix, due to large memory requirement
+        # TO-DO : Look for a way to use R matrix only.
+        r = lil_matrix((n_users+1, n_prod+1))
+        print r.shape
+        #pdb.set_trace()
+        for i in xrange(1, n_users+1):
+            # for each user, product id's he rated
+            ids = R[R[:, 0] == i, 1]
+            ratings = R[ids, 3]
+            r[i, ids] = ratings
+        # train-test split based on time-stamp
+        # Sorting based on time-stamp
+        train_idx = int(0.7 * r.shape[0])
+        #pdb.set_trace()
+        R_train = r[:train_idx, :]
+        R_test = r[train_idx:, :]
+        #pdb.set_trace()
+        return R_train, R_test, W, pf
 
 if __name__ == "__main__":
     data_handle = data_handler("../data/rating.mat",
                                "../data/trust.mat")
-    R, W = data_handle.load_matrices()
+    t = time.time()
+    R_t,R_test, W, PF = data_handle.load_matrices()
+    print time.time() - t
    # pdb.set_trace()
-    print R.shape, W.shape
+    print R_t.shape, W.shape
