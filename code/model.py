4c6f2f65c227e58385a012f653a2193bd379c79b
Shashank Gupta <shashank.gupta@research.iiit.ac.in>
2016-05-04 16:05:29 +0530

    model file updated, memerror issue

--- code/model.py
+++ code/model.py
@@ -10,10 +10,11 @@ class baseline_tensor(object):
     def __init__(self):
         # load data and initialize paramters
         data = data_handler("../data/rating.mat", "../data/trust.mat")
-        self.R, self.R1, self.W = data.load_matrices()
-        self.n_users = self.W.shape[0]
-        self.n_facets = 6
+        self.R_train, self.R_test, self.W, self.PF = data.load_matrices()
+        self.n_users, self.n_prod, self.n_cat = data.get_stats()
+        # initializing model paramters (A, B, c)
+        self.A = np.random.uniform(low=-1, high=1, size=(n_users, n_users, n_cat))
+        pdb.set_trace()
 
-
-
-
+if __name__ == "__main__":
+    model = baseline_tensor()
