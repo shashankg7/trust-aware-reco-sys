4bb05ab4a4e2aaa74c04954be27528a3cbffa3e9
Dhruv Khattar <dhruv@khattarindia.com>
2016-05-25 00:01:00 +0530

    Data Training done

--- code/data_handler.py
+++ code/data_handler.py
@@ -29,6 +29,7 @@ class data_handler():
         self.n_prod = max(R[:, 1])
         # Selecting entries with the 6 categories given in the paper
         cat_id = [7, 8, 9, 10, 11, 19]
+        cat_map = {7:0, 8:1, 9:2, 10:3, 11:4, 19:5}
         R = R[np.in1d(R[:, 2],cat_id)]
         R = R[R[:, 5].argsort()]
         R_size = R.shape[0]
@@ -36,19 +37,16 @@ class data_handler():
         R_train = R[:R_size*0.7]
         R_test = R[R_size*0.7:]
         # Making all eligible Product-Category pairs
-        prod_cat = np.unique(R_train[:, 1:3])
+        ones = np.ones(R_train.shape[0])
+        prod_cat = dict(zip(zip(R_train[:, 1], R_train[:, 2]), ones))
         # Making the mu matrix
-        mu = {}
+        mu = np.zeros(6)
         for cat in cat_id:
             cat_rating = R_train[np.where(R_train[:, 2] == cat), 3]
-            mu[cat] = np.mean(cat_rating)
-        #Converting R_train and R_test to dictionaries 
-        pdb.set_trace()
-        R_train = dict(zip(zip(R_train[:, 0],R_train[:, 1]), R_train[:, 3]))
-        R_test = dict(zip(zip(R_test[:, 0],R_test[:, 1]), R_test[:, 3]))
-        return R_train, R_test, prod_cat, mu
+            mu[cat_map[cat]] = np.mean(cat_rating)
+        return R_train, R_test, W, prod_cat, mu
             
 if __name__ == "__main__":
     data = data_handler("../data/rating_with_timestamp.mat", "../data/trust.mat")
-    R_train, R_test, PF_pair, mu = data.load_matrices()
+    R_train, R_test, W, PF_pair, mu = data.load_matrices()
     print "done"
--- code/model.py
+++ code/model.py
@@ -3,39 +3,95 @@
 import numpy as np
 from data_handler import data_handler
 import tables as tb
+import math
+import pdb
 
 class baseline_tensor():
 
     def __init__(self):
         # Loading matrices from given data
-        data = data_handler("../data/rating_with_timestamp", "../data/trust")
+        data = data_handler("../data/rating_with_timestamp.mat", "../data/trust.mat")
         self.R_train, self.R_test, self.W, self.PF, self.mu = data.load_matrices()
-        self.n_users, self.n_prod, self.n_cat = data.get_stats()    
+        # Creating R_train and R_test dictionaries
+        self.R_train_ui = dict(zip(zip(self.R_train[:, 0], self.R_train[:, 1]), self.R_train[:, 3])) 
+        self.R_test_ui = dict(zip(zip(self.R_test[:, 0], self.R_test[:, 1]), self.R_test[:, 3]))
+        # Getting unique users and products used in Training data
+        self.users = np.unique(self.R_train[:, 0])
+        self.prod = np.unique(self.R_train[:, 1])
+        self.n_users, self.n_prod, self.n_cat = data.get_stats()
         # Initializing parameters to be estimated
-        self.A = self.createA(self.n_users + 1, self.n_cat + 1)
-        self.B = np.random.rand(self.n_users + 1, self.n_cat + 1)
+        # self.A = self.createA(self.n_users + 1, self.n_cat + 1)
+        self.B = np.random.rand(self.n_users + 1, self.n_cat)
         self.C = np.random.rand(self.n_prod + 1)
-        self.E = R_train
+        self.E = self.R_train_ui
 
     def createA(self, n, c):
         f = tb.open_file('A.h5', 'w')
-        A = f.create(f.root, 'A', tb.Float32Atom(), shape = (n, n, c))
-        n_batch = math.ceil(n/1000)
+        A = f.create_carray(f.root, 'A', tb.Float32Atom(), shape = (n, n, c))
+        n_batch = int(math.ceil(n/1000))
         for i in xrange(0, n_batch):
             A[i*1000:(i+1)*1000, i*1000:(i+1)*1000, :] = np.random.uniform(low = 0, high = 1, size = (1000, 1000, c))
         return A
 
     def calc_cost(self, l):
-        cost = sum(dict[key]*dict[key] for key in self.E)
-        cost += l*(np.sum(np.dot(self.B * self.B)) + np.sum(np.dot(self.C + self.C)))
+        cost = sum(self.E[key] * self.E[key] for key in self.E)
+        cost += l*(np.vdot(self.B, self.B) + np.vdot(self.C, self.C))
         return cost
+
+    def getNui(self, u, i):
+        # Generates Users who are trusted by user u and have rated product i
+        # Users who have rated product i
+        rat_u = self.R_train[np.where(self.R_train[:, 1] == i), 0]
+        # Users trusted by u
+        trust_u = np.where(self.W[u, :] == 1)
+        return np.intersect(rat_u, trust_u)
 
     def model(self, alpha = 0.1, l = 0.1, lr_a = 0.1, lr_b = 0.1, lr_c = 0.1, n_it = 100):
         # Optimization Routine
-        self.Rcap = np.zeros_like(self.R_train)
+        self.Rcap = np.zeros_like(self.R_train_ui)
+        cat_map = {0:7, 1:8, 2:9, 3:10, 4:11, 5:19}
         for it in xrange(n_it):
-            cost = calc_cost(l)
-            
+            cost = self.calc_cost(l)
+            for u in self.users:
+                for i in self.prod:
+                    # First part of Rcap
+                    n1 = 0
+                    d1 = 0
+                    for k in xrange(self.n_cat):
+                        if (i,cat_map[k]) in self.PF:
+                            n1 += (self.mu[k] + self.B[u, k])
+                            d1 += 1
+                    Rcap = alpha * (n1/d1 + self.C[i])
+                    # Second part of Rcap
+                    n2 = 0
+                    d2 = 0
+                    v = self.getNui(u, i)
+                    for k in xrange(self.n_cat):
+                        if((i,cat_map[k]) in self.PF):
+                            n2 += np.dot(self.R_train_ui[v, i], self.A[v, u, k])
+                            d2 += np.sum(A[v, u, k])
+                    Rcap += (1 - alpha) * (n2/d2)
+                    # Defining E, p and q
+                    self.E[u, i] = R[u, i] - Rcap
+                    p = d
+                    q = n
+                    # Updating C
+                    grad_c = -alpha * self.E[u, i] + l * self.C[i]
+                    self.C[i] -= lr_c * grad_c
+                    # Updating B
+                    grad_b = -alpha * self.E[u, i] + l * (self.B[u, :]/d1)
+                    self.B[u, :] -= lr_b * grad_b
+                    # Updating A
+                    for k in xrange(self.n_cat):
+                        if (i,cat_map[k]) in self.PF:
+                            grad_a = (alpha - 1) * E[u,i] * ((p * R[v, i] - q)/(p * p))
+                            self.A[v, u, k] -= lr_a * grad_a
+                            if self.A[v, u, k] < 0:
+                                self.A[v, u, k] = 0
+                            elif self.A[v, u, k] > 1:
+                                self.A[v, u, k] = 1
+
 
 if __name__ == "__main__":
     model = baseline_tensor()
+    model.model()
